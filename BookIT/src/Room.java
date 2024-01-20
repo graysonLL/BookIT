f4aa9239a4c4f9a73aa4da6f5b4def272089ea59
graysonLL <151375618+graysonLL@users.noreply.github.com>
2024-01-20 22:58:08 +0800

    Add files via upload

---
+++ BookIT/src/Room.java
@@ -0,0 +1,38 @@
+public class Room {
+    private int roomNumber;
+    private String roomType;
+    private String status;
+
+    // Constructor
+    public Room(int roomNumber, String roomType, String status) {
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
+        this.status = status;
+    }
+
+    // Getters
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    // Setters
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ BookIT/src/User.java
@@ -0,0 +1,58 @@
+public class User {
+    private int userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+
+    // Constructor
+    public User(int userId, String firstName, String lastName, String email, String phoneNumber) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Getters
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    // Setters
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
