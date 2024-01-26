db965429f436272e63c340616aa99e6099f67cfd
graysonLL <151375618+graysonLL@users.noreply.github.com>
2024-01-26 23:25:14 +0800

    Delete bookIT2/src/User.java

--- bookIT2/src/User.java
+++
@@ -1,8 +0,0 @@
-public class User {
-    public String firstName;
-    public String lastName;
-    public String email;
-    public String phone;
-    public String password;
-    public String userId;
-}
